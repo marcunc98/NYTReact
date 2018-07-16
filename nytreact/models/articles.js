5618a1e3c60f58d17303f5fa41c16a649d76df72
Marcus Roberts <marcunc98@yahoo.com>
2018-07-16 16:59:48 -0400

    adding updates

---
+++ nytreact/models/articles.js
@@ -0,0 +1,45 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the schema costructor
+var Schema = mongoose.Schema;
+
+//Creating a Schema constructor that will create a new UserSchema object
+
+var ArticleSchema = new Schema ({
+    
+ //requiring our headline   
+    headline: {
+        type: String,
+        unique: true
+    },
+
+// requiring an article link
+    link: {
+        type: String,
+        // required: true
+    },
+
+    // requiring a summary data
+    summary: {
+        type: String,
+        // required: true
+    },
+    //note is an object that stores a Note id
+    // The ref property links the ObjectId to the Note model
+    // This allows us to populate the Article with an associated Note
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    },
+    
+    date: String,
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+//Export the Article model
+module.exports = Article;
